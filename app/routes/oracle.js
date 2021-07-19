042eb39b8874e3a90023d23dbfa773e722894abe
Yashiro <hyashir_SrvJP@landsend.com>
2021-07-19 16:18:27 +0900

    [add] oracle api

--- app/routes/oracle.js
+++ app/routes/oracle.js
@@ -2,75 +2,57 @@ var express = require('express');
 var router = express.Router();
 var oracledb = require('oracledb');
 
-router.get('/products/:product_number', function (req, res, next) {
+//Oracle DB Connection
+let conOracle = async (callBack) => {
+  let connection;
 
-  async function conOracleDB() {
-    let connection;
+  try {
+    connection = await oracledb.getConnection({
+      user: "nodeora",
+      password: "nodeora",
+      connectionString: "LEJPPDORA01:1521/orcl.leinternal.com"
+    });
 
-    try {
-      connection = await oracledb.getConnection({
-        user: "nodeora",
-        password: "nodeora",
-        connectionString: "LEJPPDORA01:1521/orcl.leinternal.com"
-      });
-      //console.log('It Works');
-      let prd_num = req.params.product_number;
-      let sql;
-      if (prd_num = 'all') {
-        sql = 'SELECT * FROM INT_PRD_MST';
-      } else {
-        sql = `SELECT * FROM INT_PRD_MST WHERE PRD_NBR = JP${prd_num}F`;
-      }
-      let result = await connection.execute(sql, [], {
-        outFormat: oracledb.OUT_FORMAT_OBJEC,
-        maxRows: 1
-      });
-      await connection.close();
+    callBack(connection);
 
-      res.status(200).send(prd_num); //result.rows
-    } catch (err) {
-      res.send('error occured: ' + err);
-    } finally {
-      if (connection) {
-        try {
-          await connection.close();
-        } catch (err) {
-          res.send(err);
-        }
+  } catch (err) {
+    res.send('connection error occured: \n' + err);
+  } finally {
+    if (connection) {
+      try {
+        await connection.close();
+      } catch (err) {
+        res.send(err);
       }
     }
   }
 
-  conOracleDB();
+}
+
+//Get style number
+router.get('/styles/:style_number', function (req, res, next) {
+
+  conOracle(async function (con) {
+    let sty_num = req.params.style_number;
+    let sql, result;
+
+    try {
+      if (sty_num == 'all') {
+        sql = 'SELECT * FROM INT_STY_MST';
+      } else {
+        sql = `SELECT * FROM INT_STY_MST WHERE STY_NBR = 'JP${sty_num}F'`;
+      }
+      result = await con.execute(sql, [], {
+        outFormat: oracledb.OUT_FORMAT_OBJEC,
+      });
+      res.status(200).send(result);
+    } catch (err) {
+      res.send("Error!!" + err);
+    }
+  })
 
 });
 
-router.post('/', function (req, res, next) {
-  /*
-  const dbPath = req.body.db;
-  const sql = req.body.sql;
-  console.log(dbPath);
-  if (!dbPath || !sql) {
-    res.send("No database path or invalid sql.")
-  }
-
-  const ADODB = require('node-adodb');
-  const connection = ADODB.open('Provider=Microsoft.ACE.OLEDB.12.0;Data Source=\\\\jpweb001\\wwwroot\\' + dbPath + ';Persist Security Info=False;');
-
-  async function execute() {
-    try {
-      let data = await connection.execute(req.body.sql)
-      res.send("the following SQL executed. \n " + res.body.sql);
-    } catch (error) {
-      console.error(error);
-      res.send(error);
-    }
-  }
-
-  execute();
-
-  // res.send("post method detected.")
-  */
-})
+router.post('/', function (req, res, next) {})
 
 module.exports = router;
