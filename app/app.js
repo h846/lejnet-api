f07694d6825b63e7203a22888856fc2b8a0a1012
Hideki Yashiro <Hideki.Yashiro@landsend.co.jp>
2021-12-02 14:42:56 +0900

    add clr_exp, smlr_item, smlr_swth API

--- app/app.js
+++ app/app.js
@@ -5,6 +5,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var logger = require('morgan');
 
+//Define Route Path 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var accdbRouter = require('./routes/accdb');
@@ -28,20 +29,17 @@ app.use(function(req, res, next){
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
-app.use(express.urlencoded({
-  extended: false
-}));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+//Route Setup
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/accdb', accdbRouter);
 app.use('/json', jsonRouter);
-app.use('/oracle', oracleRouter);
+app.use('/oracle', oracleRouter);// for L+ app
+
 //added by Janusz, 2021/11/30
 app.use('/inttool', intTool);
 
@@ -49,7 +47,6 @@ app.use('/inttool', intTool);
 app.use(function (req, res, next) {
   next(createError(404));
 });
-
 
 // error handler
 app.use(function (err, req, res, next) {
