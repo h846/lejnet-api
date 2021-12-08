acb77a785c3fcb8d4b5725340d6ec1fb208a79e6
Hideki Yashiro <Hideki.Yashiro@landsend.co.jp>
2021-12-08 14:14:57 +0900

    json api modified

--- app/routes/json.js
+++ app/routes/json.js
@@ -3,6 +3,27 @@ var express = require('express');
 var fs = require('fs');
 var router = express.Router();
 
+/* GET JSON FILE */
+router.get("/",function(req,res,next){
+   // params
+  let file = req.body.file;
+  try {
+    //Check for existence of josn file
+    let filePath = `../src/json/${file}`;
+    if (!fs.existsSync(filePath)) {
+      res.status(404).send('The requested JSON file does NOT exist').end();
+    }
+    // Load json file
+    let jsonFile = JSON.parse(fs.readFileSync(filePath, 'utf8'));
+    let jsonData = JSON.stringify(jsonFile);
+    //Send JSON file
+    res.status(200).json(jsonData).end()
+
+  } catch (e) {
+    res.status(400).send(e).end()
+  }
+})
+
 /* UPDATE JSON file*/
 // /json?file=xxxx&data={"hoge":"fuga"}
 router.put('/', function (req, res, next) {
