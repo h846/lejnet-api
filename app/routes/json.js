b397ad7f69a5e160979eb02d285686e2ed3c9d89
Hideki Yashiro <h846@outlook.com>
2021-12-09 12:59:05 +0900

    json test

--- app/routes/json.js
+++ app/routes/json.js
@@ -1,18 +1,20 @@
 // const { json } = require('express');
+const { rejects } = require('assert');
 var express = require('express');
 var fs = require('fs');
+const { resolve } = require('path');
 const { getEnvironmentData } = require('worker_threads');
 var router = express.Router();
 
-/* GET JSON FILE */
-router.get("/",function(req,res,next){
+/* MONITORING JSON FILE */
+router.get("/", async (req,res,next) => {
    // params
   let file = req.body.file;
   try {
     //Check for existence of josn file
     let filePath = `../src/json/${file}`;
     if (!fs.existsSync(filePath)) {
-      res.status(404).send('The requested JSON file does NOT exist').end();
+      res.status(404).send('file not found.');
     }
     // Load json file
     let jsonFile = JSON.parse(fs.readFileSync(filePath, 'utf8'));
@@ -22,17 +24,31 @@ router.get("/",function(req,res,next){
     res.header('Content-Type', 'application/json; charset=utf-8')
     //res.send(list['list'])
     //Send JSON file
+    /*
     setInterval((res, filePath)=>{
       await checkData(filePath)
       res.status(200).json(jsonFile.list).end()
     },1000)
+    */
+   await checkData(filePath)
+   res.send('file is updated!')
   } catch (e) {
-    res.status(400).send(e).end()
+    res.status(400).send(e)
   }
 })
-
-async function checkData(path){
-
+// Monitor the update status of JSON files.
+let checkData = (path) => {
+  return new Promise((resolve, reject)=>{
+    let date = new Date();
+    let mTime;
+    let timer = setInterval(()=>{
+      mTime = fs.statSync(path).mtime;
+      if(date.getTime() < mTime.getTime()){
+        clearInterval(timer);
+        resolve();
+      }
+    },500)
+  })
 }
 
 /* UPDATE JSON file*/
@@ -46,7 +62,7 @@ router.put('/', function (req, res, next) {
     let filePath = `../src/json/${file}`;
 
     if (!fs.existsSync(filePath)) {
-      res.status(404).send('The requested JSON file does NOT exist').end();
+      res.status(404).send('file not found.');
     }
 
     // Load json file
