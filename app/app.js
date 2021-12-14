cf07114b5dadfacdc999093de7790a755711d239
Hideki Yashiro <Hideki.Yashiro@landsend.co.jp>
2021-12-14 15:22:04 +0900

    modified top page

--- app/app.js
+++ app/app.js
@@ -11,7 +11,7 @@ var accdbRouter = require('./routes/accdb');
 var jsonRouter = require('./routes/json');
 var oracleRouter = require('./routes/oracle');
 //added by Janusz, 2021/11/30
-var intTool = require('./routes/intTool');
+//var intTool = require('./routes/intTool');
 var intOrcl = require('./routes/intOrcl');
 
 var app = express();
@@ -44,7 +44,7 @@ app.use('/accdb', accdbRouter);
 app.use('/json', jsonRouter);
 app.use('/oracle', oracleRouter);// for L+ app
 //added by Janusz, 2021/11/30
-app.use('/inttool', intTool);
+//app.use('/inttool', intTool);
 app.use('/intorcl',intOrcl);
 
 // catch 404 and forward to error handler
