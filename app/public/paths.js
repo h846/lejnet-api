f7df2c527f1b7d73bddcde825d98fc2b52917350
Janusz <janusz.przychodzen@landsend.co.jp>
2021-12-17 18:32:09 +0900

    Delete node-modules

--- app/public/paths.js
+++ app/public/paths.js
@@ -1,6 +1,7 @@
 // paths used by applications located
 // in differnet places than default /API/app
 
-const INTTOOLS_PATH = 'D:/inetpub/wwwroot/IntTool';
+//const INTTOOLS_PATH = 'D:/inetpub/wwwroot/IntTool';
+const INTTOOLS_PATH = 'C:/Projects/IntTool';
 
 module.exports = {INTTOOLS_PATH};
