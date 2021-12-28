6ba41e9ba2e4405b52ea4532eeb9cc9766d2ea6d
snishik <Shinya.Nishikawa@landsend.co.jp>
2021-12-28 12:43:03 +0900

    'fix'

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'LEJ-NET API', message: 'Welcome to LEJ-NET API' });
+  res.render('index', { title: 'CS-NET MY PAGE', message: 'Welcome to LEJ-NET API' });
 });
 
 module.exports = router;
