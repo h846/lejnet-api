5f3e962ae71b80e465b0ee064295dde9062f719a
Hideki Yashiro <h846@outlook.com>
2022-03-03 16:39:27 +0900

    cors setting

--- app/app.js
+++ app/app.js
@@ -3,6 +3,7 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+const cors = require("cors");
 
 //Define Route Path
 var indexRouter = require("./routes/index");
@@ -47,6 +48,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+app.use(
+	cors({
+		origin: "http://localhost:3000", //アクセス許可するオリジン
+		credentials: true, //レスポンスヘッダーにAccess-Control-Allow-Credentials追加
+		optionsSuccessStatus: 200, //レスポンスstatusを200に設定
+	})
+);
 
 //Route Setup
 app.use("/", indexRouter);
